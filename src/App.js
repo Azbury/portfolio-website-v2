f11132125be75b88c2c6503237be55741c5b2204
Austin Asbury <acasbury25@aol.com>
2022-11-03 14:03:27 -0400

    add frontend developer title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <div className="box-container">
         <div className="frontend-developer-container">
-  
+          <h1 className="frontend-developer-title">Frontend<br></br>Developer</h1>
         </div>
         <div className="about-me-container">
   
