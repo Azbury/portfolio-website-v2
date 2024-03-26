23ef7369d74615d1c7beb5c46ed63d2fa2d431a5
Austin Asbury <acasbury25@aol.com>
2024-03-26 02:51:52 -0400

    update profile picture

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import ProfilePicture from "../images/portfolio_profile_picture.png";
 
 class Introduction extends Component {
   render() {
     return (
-      <header>
-        <h1 id="full-name">Austin Asbury</h1>
-        <h2 id="job-title">Frontend Developer</h2>
+      <header id="header">
+        <img src={ProfilePicture} id="profile-picture"/>
+        <h1 id="full-name">AUSTIN ASBURY</h1>
+        <h2 id="job-title">Frontend Developer.</h2>
       </header>
     )
   }
