0350f17637aeb0e6ea481d35e072918631d8c757
Austin Asbury <acasbury25@aol.com>
2024-03-27 13:50:15 -0400

    adding social media buttons

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import ProfilePicture from "../images/portfolio_profile_picture.png";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 class Introduction extends Component {
   render() {
@@ -7,7 +11,13 @@ class Introduction extends Component {
       <header id="header">
         <img src={ProfilePicture} id="profile-picture"/>
         <h1 id="full-name">AUSTIN ASBURY</h1>
-        <h2 id="job-title">Frontend Developer.</h2>
+        <h2 id="job-title">Frontend Developer</h2>
+        <div id="social-media-buttons">
+          <a href="https://www.linkedin.com/in/austin-asbury/" className="social-media-button"><LinkedInIcon/></a>
+          <a href="https://github.com/Azbury" className="social-media-button"><GitHubIcon/></a>
+          <a href="https://www.youtube.com/channel/UCXQTimEpdLhoYKFTCwnX2DQ" className="social-media-button"><YouTubeIcon/></a>
+          <a href="https://www.instagram.com/yuumodrone/" className="social-media-button"><InstagramIcon/></a>
+        </div>
       </header>
     )
   }
