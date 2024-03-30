99e514031bc90cbce7b9c63321a6d5f2a57a7dd9
Austin Asbury <acasbury25@aol.com>
2024-03-30 12:36:55 -0400

    fix build errs

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,7 +4,7 @@ import Mountains from '../images/mountains.png';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
+// import InstagramIcon from '@mui/icons-material/Instagram';
 
 class Introduction extends Component {
   render() {
@@ -17,13 +17,13 @@ class Introduction extends Component {
           <a href="https://github.com/Azbury" className="social-media-button"><GitHubIcon/></a>
           <a href="https://www.youtube.com/channel/UCXQTimEpdLhoYKFTCwnX2DQ" className="social-media-button"><YouTubeIcon/></a>
         </div>
-        <img src={CartoonAustin} id="cartoon-austin"/>
+        <img src={CartoonAustin} id="cartoon-austin" alt="cartoon-austin"/>
         <div id="social-media-buttons-2">
           <a href="https://www.linkedin.com/in/austin-asbury/" className="social-media-button"><LinkedInIcon/></a>
           <a href="https://github.com/Azbury" className="social-media-button"><GitHubIcon/></a>
           <a href="https://www.youtube.com/channel/UCXQTimEpdLhoYKFTCwnX2DQ" className="social-media-button"><YouTubeIcon/></a>
         </div>
-        <img src={Mountains} id="mountains"/>
+        <img src={Mountains} id="mountains" alt="mountains"/>
         <div id="below-mountains"></div>
       </header>
     )
