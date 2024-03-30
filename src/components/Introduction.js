fe49961426f8bbf0bd65cd3bc6fa5a4f51833e70
Austin Asbury <acasbury25@aol.com>
2024-03-30 12:31:52 -0400

    new landing page image

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
+import CartoonAustin from '../images/cartoon_austin.png';
 import Mountains from '../images/mountains.png';
-import ProfilePicture from "../images/portfolio_profile_picture.png";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -10,16 +10,21 @@ class Introduction extends Component {
   render() {
     return (
       <header id="header">
-        <img src={ProfilePicture} id="profile-picture"/>
         <h1 id="full-name">AUSTIN ASBURY</h1>
         <h2 id="job-title">Frontend Developer</h2>
         <div id="social-media-buttons">
           <a href="https://www.linkedin.com/in/austin-asbury/" className="social-media-button"><LinkedInIcon/></a>
           <a href="https://github.com/Azbury" className="social-media-button"><GitHubIcon/></a>
           <a href="https://www.youtube.com/channel/UCXQTimEpdLhoYKFTCwnX2DQ" className="social-media-button"><YouTubeIcon/></a>
-          <a href="https://www.instagram.com/yuumodrone/" className="social-media-button"><InstagramIcon/></a>
+        </div>
+        <img src={CartoonAustin} id="cartoon-austin"/>
+        <div id="social-media-buttons-2">
+          <a href="https://www.linkedin.com/in/austin-asbury/" className="social-media-button"><LinkedInIcon/></a>
+          <a href="https://github.com/Azbury" className="social-media-button"><GitHubIcon/></a>
+          <a href="https://www.youtube.com/channel/UCXQTimEpdLhoYKFTCwnX2DQ" className="social-media-button"><YouTubeIcon/></a>
         </div>
         <img src={Mountains} id="mountains"/>
+        <div id="below-mountains"></div>
       </header>
     )
   }
