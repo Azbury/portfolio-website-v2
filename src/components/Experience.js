06a88f11511c7e28e8e27f3b02ea6e5c8d552f20
Austin Asbury <acasbury25@aol.com>
2024-06-12 21:39:27 -0400

    complete experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ReactComponent as Trees } from '../images/trees.svg';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 
 class Experience extends Component {
   render() {
@@ -8,15 +9,35 @@ class Experience extends Component {
         <Trees/>
         <div id="experience-inner">
           <h3 id="experience-header">EXPERIENCE</h3>
-          <p id="experience-description">
-          Hey there! I'm Austin, a web developer who finds joy in crafting elegant code 
-          and exploring the great outdoors. I'm as passionate about coding as I am about 
-          embarking on adventures, whether it's hiking through rugged landscapes, camping 
-          under the stars, or hitting the slopes in winter. My love for nature fuels my 
-          creativity, bringing fresh perspectives to my work. I thrive in environments 
-          where innovation meets challenge, constantly pushing boundaries and seeking new 
-          heights. Let's connect and embark on this exciting journey together!
-          </p>
+          <div className="experience-section">
+            <span className="experience-years">2020 <code>&#8212;</code> Present</span>
+            <a className="experience-title" href="https://ritterim.com/" target="_blank" rel="noreferrer">UX/UI Developer - Ritter Insurance Marketing <ArrowOutwardIcon fontSize="small"/></a>
+            <span className="experience-title">Jr. UX/UI Developer</span>
+            <ul className="experience-description">
+              <li>
+                Lead front-end developer for a contract system developed with Vue.js 2, enabling insurance
+                agents to select, answer questions, and electronically sign contracts. Managed the
+                integration of these contracts into the company database and developed a user-friendly
+                interface for internal processing.
+              </li>
+              <li>
+                Lead front-end developer for a call recording system, equipping insurance agents with the
+                capability to record inbound and outbound calls, enhancing compliance and customer service standards.
+              </li>
+              <li>
+                Created other various Vue.js 2 applications integrated into "The Platform,"the company's
+                primary website, enriching its functionality and user experience.
+              </li>
+              <li>
+                Successfully converted the primary company website,"The Platform,"from Vue.js 2 to
+                Vue.js 3, overseeing the migration of numerous Vue.js 2 applications within the site.
+              </li>
+              <li>
+                Maintained and optimized static site generators (SSGs) initially built with Hugo, and later
+                migrated them to Astro for enhanced performance and flexibility
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     )
