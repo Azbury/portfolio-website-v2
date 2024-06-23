8e2e5784aeadeca25eab7df66ba18f2c6bddb237
Austin Asbury <acasbury25@aol.com>
2024-06-23 18:39:17 -0400

    add description and video to osrs bot

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,10 +13,22 @@ class Projects extends Component {
             <a className="projects-title" href="https://github.com/Azbury/osrs-bot" target="_blank" rel="noreferrer">
               Old School RuneScape Scripting Bot<ArrowOutwardIcon fontSize="small"/>
             </a>
-            <p>test</p>
-            <a className="projects-title" href="https://github.com/Azbury/osrs-bot" target="_blank" rel="noreferrer">
-              Old School RuneScape Scripting Bot <ArrowOutwardIcon fontSize="small"/>
-            </a>
+            <p>
+              An Old School RuneScape scripting bot built with JavaScript designed to automate in-game tasks through 
+              the game's web client. This scripting bot mimics human actions such as agility training, combat, or fire making,
+              allowing players to accumulate resources or experience without actively playing.
+            </p>
+            <iframe 
+              width="100%" 
+              height="315" 
+              src="https://www.youtube.com/embed/kNOEWbzpY90?si=BnW5IDf1zoxrdF7q" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerpolicy="strict-origin-when-cross-origin" 
+              allowfullscreen="true"
+            >
+            </iframe>
           </div>
         </div>
       </div>
