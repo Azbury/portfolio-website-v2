34be76919816d40b025a6d0f85ecb39debcd3093
Austin Asbury <acasbury25@aol.com>
2024-08-11 22:11:19 -0400

    fix typo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,7 +38,7 @@ class Projects extends Component {
                 </a>
                 <p>
                   This is a game built using React.js, HTML, CSS, with a Ruby on Rails API for the backend. The game allows a user to enter a 
-                  username and take them to their user page. They can they either start a new game or view the leaderboard page for the game. 
+                  username and take them to their user page. They can either start a new game or view the leaderboard page for the game. 
                   The game is an card memory matching game where they are given the time view the position of cards and then they have to flip 
                   them over and match them from their memory. A user gains 10 points for every correct answer, and loses 10 points for every wrong answer.
                 </p>
